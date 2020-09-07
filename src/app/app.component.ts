f46f9a6f2d5f00a3a8d150a9d87a893ee56ca50a
luis.orona <luis.orona@destina.mx>
2020-09-07 17:32:50 -0500

    otro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import {Link} from './link/link.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-links-votes-app';
+
+  links: Link[];
+  constructor(){
+    this.links= [
+      new Link('Angular', 'https://angular.io',100),
+      new Link('React', 'https://reactjs.org/',200),
+      new Link('Vue', 'https://vuejs.org/',50)
+    ]
+    console.log(this.links);
+  }
+  addLink(title: HTMLInputElement, link: HTMLInputElement) {
+    this.links.push(new Link(title.value,link.value));
+    title.value='';
+    link.value='';
+    return false;
+  }
 }
